413e05228e826ac7ae675423f46c916f76adf0d0
Ekubiat <ekubiateka@gmail.com>
2025-03-11 13:24:46 +0100

    Assignment

---
+++ Assignment.js
@@ -0,0 +1,28 @@
+
+// Declaring two variables and assign values
+let num1 = 70;
+let num2 = 7;
+
+// Perform operations
+//let addition = num1 + num2;
+let addition = 70 + 7;
+
+//let subtraction = num1 - num2;
+let subtraction = 70 - 7;
+
+//let multiplication = num1 * num2;
+let multiplication = 70 * 7;
+
+//let division = num1 / num2;
+let division = 70 / 7;
+
+//let modulus = num1 % num2;
+let modulus = 70 % 7;
+
+
+// Print results
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log("First JavaScript")
+
+
+var name = "ANST"
+console.log(name)
+
+//var name = 31
+///console.log(name)
